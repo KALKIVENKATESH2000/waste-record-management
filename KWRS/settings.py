4d4ed62159d26c271e6c99ce90cbc600fa84a53a
Venkata Rao <60035982+venkat-ksnp@users.noreply.github.com>
2023-06-28 19:48:53 +0530

    Update settings.py

--- KWRS/settings.py
+++ KWRS/settings.py
@@ -38,12 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'master',
-    # 'crispy_forms',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
+    'django.contrib.sites'
 ]
 
 MIDDLEWARE = [
