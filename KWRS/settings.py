6384ede9d36de1c7c82211f025d21c879d20d1ba
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-07-05 12:04:10 +0530

    datepicker change

--- KWRS/settings.py
+++ KWRS/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'master',
-    'django.contrib.sites'
+    'django.contrib.sites',
+    'allauth',
 ]
 
 MIDDLEWARE = [
