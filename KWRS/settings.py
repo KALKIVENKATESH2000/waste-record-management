e3d0afc4f30e6451c83031edec572cb334f536ab
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-08-23 17:37:05 +0530

    set

--- KWRS/settings.py
+++ KWRS/settings.py
@@ -135,6 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "master.CustomUser"
+# AUTH_PASSWORD_VALIDATORS
 
 # APPEND_SLASH = False
 SITE_ID = 2
