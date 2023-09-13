c222a121b8c83f57c7d45ee1e819aa6805a9d2e2
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-09-13 14:49:02 +0530

    db configuration

--- KWRS/settings.py
+++ KWRS/settings.py
@@ -78,10 +78,21 @@ AUTH_USER_MODEL  = 'master.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Kaysim_wrs',
+        'USER': 'WRS',
+        'PASSWORD': 'Kaysim@wrs1',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
